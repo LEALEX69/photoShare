bbbbd7b1eb9a2122b2908e68784191baec13d31c
LEALEX69 <alexandre.le1908@gmail.com>
2024-11-28 15:35:21 +0100

    feat: add initDb.sql base

---
+++ initDb.sql
@@ -0,0 +1,23 @@
+drop database photoShare;
+-- drop table user;
+use photoShare;
+
+/* CREATE TABLE user (
+                      id INT AUTO_INCREMENT PRIMARY KEY,
+                      last_name VARCHAR(100) NOT NULL,
+                      first_name VARCHAR(100) NOT NULL,
+                      email VARCHAR(100) NOT NULL,
+                      birth_date DATE
+); */
+
+CREATE TABLE image {
+    idPhoto AUTO_INCREMENT PRIMARY KEY,
+    uploadDate DATE NOT NULL,
+    description VARCHAR(255),
+    imagePath VARCHAR(255)
+};
+
+-- INSERT INTO user VALUES (1, "ASSAHI", "ABD", "abd@gmail.com", '2003-10-31');
+-- INSERT INTO user VALUES (2, "KHARFI", "Adam", "adam@gmail.com", '2003-07-28');
+
+-- select * from user;
