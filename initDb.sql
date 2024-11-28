333657f3fe2b168f165fc607a1cec5c9bbfc5db6
LEALEX69 <alexandre.le1908@gmail.com>
2024-11-28 15:39:52 +0100

    fix: fix underscore missing

--- initDb.sql
+++ initDb.sql
@@ -10,12 +10,12 @@ use photoShare;
                       birth_date DATE
 ); */
 
-CREATE TABLE image {
+CREATE TABLE image (
     idImage AUTO_INCREMENT PRIMARY KEY,
     uploadDate DATE NOT NULL,
     description VARCHAR(255),
     imagePath VARCHAR(255)
-};
+    );
 
 -- INSERT INTO user VALUES (1, "ASSAHI", "ABD", "abd@gmail.com", '2003-10-31');
 -- INSERT INTO user VALUES (2, "KHARFI", "Adam", "adam@gmail.com", '2003-07-28');
