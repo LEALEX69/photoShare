18510e08f96c98066e5233da7400ac73a91b497a
LE Alexandre <alexandre.le@etu.univ-lyon1.fr>
2024-11-28 14:28:35 +0100

    initial commit

---
+++ src/main/java/local/epul4a/photoshare/PhotoShareApplication.java
@@ -0,0 +1,13 @@
+package local.epul4a.photoshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoShareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/local/epul4a/photoshare/PhotoShareApplicationTests.java
@@ -0,0 +1,13 @@
+package local.epul4a.photoshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
