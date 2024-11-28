3b4bb64bd45d47cf80a3eb266883c6c48cd27729
LEALEX69 <alexandre.le1908@gmail.com>
2024-11-28 15:22:46 +0100

    feat: implement base code

--- src/main/java/local/epul4a/photoshare/PhotoShareApplication.java
+++ src/main/java/local/epul4a/photoshare/PhotoShareApplication.java
@@ -1,13 +1,26 @@
 package local.epul4a.photoshare;
 
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import local.epul4a.photoshare.service.FilesStorageService;
+
 @SpringBootApplication
-public class PhotoShareApplication {
+public class PhotoShareApplication implements CommandLineRunner {
+
+    @Resource
+    FilesStorageService storageService;
 
     public static void main(String[] args) {
         SpringApplication.run(PhotoShareApplication.class, args);
     }
 
+    @Override
+    public void run(String... arg) throws Exception {
+//    storageService.deleteAll();
+        storageService.init();
+    }
 }
